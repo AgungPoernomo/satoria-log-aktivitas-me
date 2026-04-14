80835ab9b29868dd5bd8bf7af3d67e844f9fd3b7
Lana2331 <agungdwicahyo2001@gmail.com>
2026-04-14 13:32:58 +0700

    Refactor: v3

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // !! PERINGATAN !!
-    // Ini akan mengabaikan error TypeScript saat proses build di Vercel
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    // Mengabaikan peringatan ESLint saat build
-    ignoreDuringBuilds: true,
+    ignoreBuildErrors: true, // Biarkan ini tetap ada
   },
 };
 
